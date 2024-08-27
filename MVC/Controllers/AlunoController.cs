0c82bdbb22063e50d194d838fb8a676d17e53f82
Tiago Pereira <tiagopereiraduarte8@gmail.com>
2024-08-27 14:28:10 -0300

    finalizado a lista de alunos

--- MVC/Controllers/AlunoController.cs
+++ MVC/Controllers/AlunoController.cs
@@ -7,11 +7,11 @@ namespace MVC.Controllers
     {
         public IActionResult Index()
         {
-            AlunoController a1 = new Aluno(1, "Tiago Pereira", "tiagopereira@teste.com.br", 123456);
-            AlunoController a2 = new Aluno(2, "Januario silva", "januariosilva@teste.com.br", 223344);
-            AlunoController a3 = new Aluno(3, "Nobru Goes", "nobrugoes@teste.com.br", 333666);
-            AlunoController a4 = new Aluno(4, "Cristiano Ronaldo", "cr7thegoat@teste.com.br", 444444);
-            AlunoController a5 = new Aluno(5, "Cesar costa", "cesarcosta@teste.com.br", 557788);
+            Aluno a1 = new Aluno(1, "Tiago Pereira", "tiagopereira@teste.com.br", 123456);
+            Aluno a2 = new Aluno(2, "Januario silva", "januariosilva@teste.com.br", 223344);
+            Aluno a3 = new Aluno(3, "Nobru Goes", "nobrugoes@teste.com.br", 333666);
+            Aluno a4 = new Aluno(4, "Cristiano Ronaldo", "cr7thegoat@teste.com.br", 444444);
+            Aluno a5 = new Aluno(5, "Cesar costa", "cesarcosta@teste.com.br", 557788);
 
             List<Aluno> listaAlunos = new List<Aluno>();
 
